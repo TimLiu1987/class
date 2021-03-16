bd5497918afe88f18c45addfac5594c114c01d7b
TimLiu1987 <59197061+TimLiu1987@users.noreply.github.com>
2021-03-16 10:50:50 +0800

    Create main.cpp

---
+++ 9-6/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Score.h"
+using namespace std;
+
+int main()
+{
+    string name;
+    int math;
+
+    cin >> name >> math;
+
+    Score score1("John", 90);
+    Score score2("Mary", 80);
+
+    score1.setName(name);
+    score2.setMath(math);
+
+    score1.print();
+    score2.print();
+}
