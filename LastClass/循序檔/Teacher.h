85b54be062bcc47bfa882beea366ee8fcc448d86
TimLiu1987 <59197061+TimLiu1987@users.noreply.github.com>
2021-06-14 21:58:51 +0800

    Create Teacher.h

---
+++ LastClass/循序檔/Teacher.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Teacher
+{
+private:
+	string name;
+	int age;
+	double score;
+
+public:
+	Teacher();
+	Teacher(string, int, double);
+	void setName(string);
+	void setAge(int);
+	void setScore(double);
+	string getName();
+	int getAge();
+	double getScore();
+
+	void writeToFile();
+	void readFromFile();
+	
+};
+
